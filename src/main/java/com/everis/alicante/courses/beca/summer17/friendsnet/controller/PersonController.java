02a44efacc7e04bd82830b558e444054c84e3cca
franperiagoliver <franperiagoliver@gmail.com>
2017-08-24 17:53:59 +0200

    PersonController created.

--- src/main/java/com/everis/alicante/courses/beca/summer17/friendsnet/controller/PersonController.java
+++ src/main/java/com/everis/alicante/courses/beca/summer17/friendsnet/controller/PersonController.java
@@ -1,5 +1,90 @@
+/*
+ * Created at 24-ago-2017 by Fran Periago.
+ */
 package com.everis.alicante.courses.beca.summer17.friendsnet.controller;
 
+import com.everis.alicante.courses.beca.summer17.friendsnet.dao.interfaces.PersonDAO;
+import com.everis.alicante.courses.beca.summer17.friendsnet.entity.Person;
+import com.everis.alicante.courses.beca.summer17.friendsnet.manager.PersonManager;
+
+/**
+ * The Class PersonController.
+ */
 public class PersonController {
 	
+	/** The person manager. */
+	private PersonManager personManager;
+	
+	/**
+	 * Instantiates a new person controller.
+	 *
+	 * @param personDao the person dao
+	 */
+	public PersonController(final PersonDAO personDao) {
+		this.personManager = new PersonManager(personDao);
+	}
+	
+	/**
+	 * Save person.
+	 *
+	 * @param person the person
+	 */
+	public void savePerson(final Person person) {
+		this.personManager.savePerson(person);
+	}
+	
+	/**
+	 * Save persons.
+	 *
+	 * @param persons the persons
+	 */
+	public void savePersons(final Iterable<Person> persons) {
+		this.personManager.savePersons(persons);
+	}
+	
+	/**
+	 * Update person.
+	 *
+	 * @param person the person
+	 */
+	public void updatePerson(final Person person) {
+		this.personManager.updatePerson(person);
+	}
+	
+	/**
+	 * Update persons.
+	 *
+	 * @param persons the persons
+	 */
+	public void updatePersons(final Iterable<Person> persons) {
+		this.personManager.updatePersons(persons);
+	}
+	
+	/**
+	 * Removes the person.
+	 *
+	 * @param person the person
+	 */
+	public void removePerson(final Person person) {
+		this.personManager.removePerson(person);
+	}
+	
+	/**
+	 * Find persons.
+	 *
+	 * @return the iterable
+	 */
+	public Iterable<Person> findPersons() {
+		return this.personManager.findPersons();
+	}
+	
+	/**
+	 * Find person by id.
+	 *
+	 * @param id the id
+	 * @return the person
+	 */
+	public Person findPersonById(Long id) {
+		return this.personManager.findPersonById(id);
+	}
 }
--- src/main/java/com/everis/alicante/courses/beca/summer17/friendsnet/manager/PersonManager.java
+++ src/main/java/com/everis/alicante/courses/beca/summer17/friendsnet/manager/PersonManager.java
@@ -1,41 +1,89 @@
+/*
+ * Created at 24-ago-2017 by Fran Periago
+ */
 package com.everis.alicante.courses.beca.summer17.friendsnet.manager;
 
 import com.everis.alicante.courses.beca.summer17.friendsnet.dao.interfaces.PersonDAO;
 import com.everis.alicante.courses.beca.summer17.friendsnet.entity.Person;
 
+/**
+ * The Class PersonManager.
+ */
 public class PersonManager {
 	
-	PersonDAO personDao;
+	/** The person dao. */
+	private PersonDAO personDao;
 	
+	/**
+	 * Instantiates a new person manager.
+	 *
+	 * @param personDao the person dao
+	 */
 	public PersonManager(final PersonDAO personDao) {
 		super();
 		this.personDao = personDao;
 	}
 	
+	/**
+	 * Save person.
+	 *
+	 * @param person the person
+	 */
 	public void savePerson(final Person person) {
 		this.personDao.save(person);
 	}
 	
+	/**
+	 * Save persons.
+	 *
+	 * @param persons the persons
+	 */
 	public void savePersons(final Iterable<Person> persons) {
 		this.personDao.save(persons);
 	}
 	
+	/**
+	 * Update person.
+	 *
+	 * @param person the person
+	 */
 	public void updatePerson(final Person person) {
 		this.personDao.update(person);
 	}
 	
+	/**
+	 * Update persons.
+	 *
+	 * @param persons the persons
+	 */
 	public void updatePersons(final Iterable<Person> persons) {
 		this.personDao.update(persons);
 	}
 	
+	/**
+	 * Removes the person.
+	 *
+	 * @param person the person
+	 */
 	public void removePerson(final Person person) {
 		this.personDao.remove(person);
 	}
 	
+	/**
+	 * Find persons.
+	 *
+	 * @return the iterable
+	 */
 	public Iterable<Person> findPersons() {
 		return this.personDao.findAll();
 	}
 	
+	/**
+	 * Find person by id.
+	 *
+	 * @param id the id
+	 * @return the person
+	 */
 	public Person findPersonById(Long id) {
 		return this.personDao.findById(id);
 	}
